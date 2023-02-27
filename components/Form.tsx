7b63973d0672d0045ed768cb76de86e899f1edc6
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2023-02-26 22:00:24 -0300

    feat: add header

---
+++ components/Form.tsx
@@ -0,0 +1,38 @@
+import form from '../styles/Form.module.css'
+import Image from 'next/image'
+
+export default function Form() {
+
+  function MyImage(src: any, alt: any) {
+    return (
+      <Image
+        src={src}
+        alt={alt}
+        width={24}
+        height={24}
+        className={form.icon}
+      />
+    )
+  }
+
+  return(
+    <div className={form.container_form}>
+    <form className={form.form}>
+      <fieldset className={form.search}>
+        <Image className={form.icon} src={'/search.svg'} alt={'Busca por nome'} width={24} height={24}/>
+        <input type="text" placeholder='Pesquise por nome'/>
+      </fieldset>
+      <fieldset className={form.location}>
+        <Image className={form.icon_location} src={'/location.svg'} alt={'Busca por cidade'} width={24} height={24}/>
+        <select className={form.option} name='selectCity'>
+          <option value="Selecione uma cidade">Selecione uma cidade</option>
+          <option value="SC">SC</option>
+          <option value="SP">SP</option>
+          <option value="RS">RS</option>
+        </select>
+      </fieldset>
+      <button className={form.button}>Buscar Agora</button>
+    </form>
+  </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
+import home from '../styles/Home.module.css'
+import Form from '@/components/Form'
+
 export default function Home() {
   return (
     <>
-    <div>HOME</div>
+      <header className={home.header}>
+        <div className={home.container_search}>
+          <h2 className={home.title}>Find Your Block</h2>
+          <p className={home.blocks}>Encontre os <span className={home.blocks_top}>melhores blocos</span> de carnaval de 2023</p>
+        </div> 
+
+        <Form />
+      </header>
+
     </>
   )
 }
